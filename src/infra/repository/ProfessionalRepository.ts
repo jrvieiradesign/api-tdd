490dffb68b347d25a6c0087b716d438317539dd9
Zaqueu Junior <zaqueujunior1998@gmail.com>
2025-03-25 14:24:20 -0300

    feat: implement save repositories

--- src/infra/repository/ProfessionalRepository.ts
+++ src/infra/repository/ProfessionalRepository.ts
@@ -9,8 +9,12 @@ export default interface ProfessionalRepository {
 
 
 export class ProfessionalRepositoryDatabase implements ProfessionalRepository {
-    saveProfessional(professional: Professional): Promise<void> {
-        throw new Error("Method not implemented.");
+    async saveProfessional(professional: Professional): Promise<void> {
+        const connection = pgp()("postgres://postgres:postgre@localhost:5432/api_tdd");
+        await connection.query("insert into professional (professional_id, name, email, position, salary) values ($1, $2, $3, $4, $5)",
+            [professional.professionalId, professional.name, professional.getEmail(), professional.position, professional.salary]
+        )
+        await connection.$pool.end()
     }
     updateProfessional(professional: Professional): Promise<void> {
         throw new Error("Method not implemented.");
